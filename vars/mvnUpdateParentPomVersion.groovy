4e483fba2f70f060565daf1a72253c0226e51d13
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-08-02 00:30:24 +0200

    kick out MF_PARENT_VERSION etc
    See the javadoc at
    metasfresh-jenkins-pipeline/vars/mvnUpdateParentPomVersion.groovy for
    details
    
    maven change "local" version from 1.0.0 to 3.0.0
    https://github.com/metasfresh/metasfresh/issues/2102

--- vars/mvnUpdateParentPomVersion.groovy
+++ vars/mvnUpdateParentPomVersion.groovy
@@ -8,27 +8,22 @@ import de.metas.jenkins.MvnConf;
 // * https://jenkins.io/doc/book/pipeline/shared-libraries/
 
 /**
-  * Makes sure that the parent pom declared within the given {@code mvnConf}'s {@code pomFile} points to the given {@code newParentVersion}
+  * Makes sure that the parent pom declared within the given {@code mvnConf}'s {@code pomFile} is resolved to a particular version.
+  * Note that we can't reliably resolve the range to *a particular* version.
+  * 'resolve-ranges' has no parameter to specify a version and
+  * 'update-parent' happens to only to its job if the 'parentVersion' values is *not the latest one*.
+  * Therefore and becauwse metasfresh-parent is a slow-moving target, we don't insist on controlling it to particular version.
   */
-def call(final MvnConf mvnConf, final String newParentVersion='LATEST')
+def call(final MvnConf mvnConf)
 {
-    //echo "mvnUpdateParentPomVersion is called with newParentVersion=${newParentVersion} and mvnConf=${mvnConf}"
+    echo "mvnUpdateParentPomVersion is called with newParentVersion=${newParentVersion} and mvnConf=${mvnConf}"
 
     // make sure we know which plugin version we run
     final String pluginGAV='org.codehaus.mojo:versions-maven-plugin:2.4'
 
-    if(newParentVersion && newParentVersion!='LATEST')
-    {
-       // update the parent pom version. Since https://github.com/metasfresh/metasfresh/issues/2102 we have a parent version *range*, therefore, we don't use "update-parent" anymore, but "resolve-ranges"
-       echo "mvnUpdateParentPomVersion: Update the parent pom version to the explicitly given value ${newParentVersion}"
-       sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --non-recursive --batch-mode -DallowSnapshots=false -DgenerateBackupPoms=true ${mvnConf.resolveParams} -DparentVersion=[${newParentVersion}] ${pluginGAV}:update-parent"
-    }
-    else
-    {
-       // this method is *just* about the parent pom. don't do unexpected additional stuff
-       final String processOnlyParentParams="-DprocessParent=true -DprocessDependencies=false -DprocessDependencyManagement=false -DprocessProperties=false"
+    // this method is *just* about the parent pom. don't do unexpected additional stuff
+    final String processOnlyParentParams="-DprocessParent=true -DprocessDependencies=false -DprocessDependencyManagement=false -DprocessProperties=false"
 
-       echo "mvnUpdateParentPomVersion: Resolve the parent version range"
-       sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --non-recursive --batch-mode -DallowSnapshots=false -DgenerateBackupPoms=true ${processOnlyParentParams} ${mvnConf.resolveParams} ${pluginGAV}:resolve-ranges"
-     }
+    echo "mvnUpdateParentPomVersion: Resolve the parent version range"
+    sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --non-recursive --batch-mode -DallowSnapshots=false -DgenerateBackupPoms=true ${processOnlyParentParams} ${mvnConf.resolveParams} ${pluginGAV}:resolve-ranges"
 }
