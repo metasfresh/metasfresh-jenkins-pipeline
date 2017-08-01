d2b56e7a09c87bad2668f12d1d93ce2feac9beb5
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-08-01 10:51:08 +0200

    try to pass parameters to the library
    struggling with org.jenkinsci.plugins.scriptsecurity.sandbox.RejectedAccessException
    maven change "local" version from 1.0.0 to 3.0.0 https://github.com/metasfresh/metasfresh/issues/2102

--- src/de/metas/jenkins/MvnConf.groovy
+++ src/de/metas/jenkins/MvnConf.groovy
@@ -3,6 +3,42 @@ package de.metas.jenkins;
 class MvnConf
 {
 	String pomfile;
+
+	/**
+	  * String containing the settings file, to be contatenated to a "--settings .." parameter
+		*/
 	String settingsFile;
+
+	/**
+	  * String containing a number of "-Dtask-repo-.." maven parameters, used to resolve dependencies from the repos that we want to resolve from
+	  */
 	String resolveParams;
+
+	/**
+	  * String containing a "-DaltDeploymentRepository=.." maven parameter, used to deploy dependencies to the repo which we want to deploy to
+		*/
+	String deployParam;
+
+	String getPomFileParam()
+	{
+		return "--file ${pomFile}"
+	}
+
+	/**
+	  * @return string containing a "--settings .." parameter, used to tell maven which settings to use
+		*/
+	String getSettingsFileParam()
+	{
+		return "--file ${settingsFile}"
+	}
+
+	String getDeployParam()
+	{
+		return deployParam;
+	}
+
+	String getResolveParams()
+	{
+		return resolveParams;
+	}
 }
