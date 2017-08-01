c9e130c2db9b214a392e4248cbaa05f115cd4cbc
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-08-01 23:00:06 +0200

    control the versions plugin version
    maven change "local" version from 1.0.0 to 3.0.0
    https://github.com/metasfresh/metasfresh/issues/2102

--- vars/mvnUpdateParentPomVersion.groovy
+++ vars/mvnUpdateParentPomVersion.groovy
@@ -12,13 +12,16 @@ import de.metas.jenkins.MvnConf;
   */
 def call(final MvnConf mvnConf, final String newParentVersion='LATEST')
 {
-    echo "mvnUpdateParentPomVersion is called with newParentVersion=${newParentVersion} and mvnConf=${mvnConf}"
+    //echo "mvnUpdateParentPomVersion is called with newParentVersion=${newParentVersion} and mvnConf=${mvnConf}"
+
+    // make sure we know which plugin version we run
+    final String pluginGAV='org.codehaus.mojo:versions-maven-plugin:2.4'
 
     if(newParentVersion && newParentVersion!='LATEST')
     {
        // update the parent pom version. Since https://github.com/metasfresh/metasfresh/issues/2102 we have a parent version *range*, therefore, we don't use "update-parent" anymore, but "resolve-ranges"
        echo "mvnUpdateParentPomVersion: Update the parent pom version to the explicitly given value ${newParentVersion}"
-       sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --non-recursive --batch-mode -DallowSnapshots=false -DgenerateBackupPoms=true ${mvnConf.resolveParams} -DparentVersion=[${newParentVersion}] versions:update-parent"
+       sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --non-recursive --batch-mode -DallowSnapshots=false -DgenerateBackupPoms=true ${mvnConf.resolveParams} -DparentVersion=[${newParentVersion}] ${pluginGAV}:update-parent"
     }
     else
     {
@@ -26,7 +29,6 @@ def call(final MvnConf mvnConf, final String newParentVersion='LATEST')
        final String processOnlyParentParams="-DprocessParent=true -DprocessDependencies=false -DprocessDependencyManagement=false -DprocessProperties=false"
 
        echo "mvnUpdateParentPomVersion: Resolve the parent version range"
-       echo "Going to call mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --non-recursive --batch-mode -DallowSnapshots=false -DgenerateBackupPoms=true ${processOnlyParentParams} ${mvnConf.resolveParams} versions:resolve-ranges"
-       sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --non-recursive --batch-mode -DallowSnapshots=false -DgenerateBackupPoms=true ${processOnlyParentParams} ${mvnConf.resolveParams} versions:resolve-ranges"
+       sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --non-recursive --batch-mode -DallowSnapshots=false -DgenerateBackupPoms=true ${processOnlyParentParams} ${mvnConf.resolveParams} ${pluginGAV}:resolve-ranges"
      }
 }
