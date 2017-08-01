74ae8cf8478fd972934b1453b6aa3ad861411027
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-08-01 11:14:20 +0200

    MvnConf: cleanup and typo-fix; also using it again
    maven change "local" version from 1.0.0 to 3.0.0 https://github.com/metasfresh/metasfresh/issues/2102

--- src/de/metas/jenkins/MvnConf.groovy
+++ src/de/metas/jenkins/MvnConf.groovy
@@ -2,7 +2,10 @@ package de.metas.jenkins;
 
 class MvnConf
 {
-	String pomfile;
+	/**
+	  * String containing the pom file, to be concatenated to a "--file .." parameter
+	  */
+	String pomFile;
 
 	/**
 	  * String containing the settings file, to be contatenated to a "--settings .." parameter
@@ -18,27 +21,4 @@ class MvnConf
 	  * String containing a "-DaltDeploymentRepository=.." maven parameter, used to deploy dependencies to the repo which we want to deploy to
 		*/
 	String deployParam;
-
-	String getPomFileParam()
-	{
-		return "--file ${pomFile}"
-	}
-
-	/**
-	  * @return string containing a "--settings .." parameter, used to tell maven which settings to use
-		*/
-	String getSettingsFileParam()
-	{
-		return "--file ${settingsFile}"
-	}
-
-	String getDeployParam()
-	{
-		return deployParam;
-	}
-
-	String getResolveParams()
-	{
-		return resolveParams;
-	}
 }
--- vars/mvnUpdateParentPomVersion.groovy
+++ vars/mvnUpdateParentPomVersion.groovy
@@ -1,14 +1,13 @@
 #!/usr/bin/groovy
 
-//import de.metas.jenkins.MvnConf;
+import de.metas.jenkins.MvnConf;
 
 // many thanks to
 // * https://github.com/fabric8io/jenkins-pipeline-library#git-tag
 // * https://github.com/fabric8io/fabric8-pipeline-library/blob/master/vars/gitTag.groovy
 // * https://jenkins.io/doc/book/pipeline/shared-libraries/
 
-
-def call(final Map mvnConf, final String newParentVersion='LATEST')
+def call(final MvnConf mvnConf, final String newParentVersion='LATEST')
 {
     echo """mvnUpdateParentPomVersion is called with
 \tmvnConf=${mvnConf}
