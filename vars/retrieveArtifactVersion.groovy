f0129b15788dacd4dcbcda352792c8b229696509
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-08-11 17:57:10 +0200

    add another step to make the version string
    maven change "local" version from 1.0.0 to 3.0.0 https://github.com/metasfresh/metasfresh/issues/2102

---
+++ vars/retrieveArtifactVersion.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/groovy
+
+
+String call(final String branchName, final int buildNo)
+{
+	final String majorAndMinorVersion = retrieveReleaseInfo(branchName);
+	echo "Retrieved MajorVersion.MinorVersion=${majorAndMinorVersion}"
+
+	// set the version prefix, 1 for "master", 2 for "not-master" a.k.a. feature
+	final patchVersion = branchName.equals('master') ? "1" : "2"
+	echo "Set PatchVersion=${patchVersion}"
+	
+	final String artifactVersion="${majorAndMinorVersion}.${patchVersion}-${buildNo}+{branchName}"
+	echo "Set artifactVersion={artifactVersion}"
+
+	return artifactVersion
+}
