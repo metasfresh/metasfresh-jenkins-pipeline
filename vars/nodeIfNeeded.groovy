c1b193fdca221eebdba5546440f06a2c063905ec
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-08-19 02:47:10 +0200

    use nodeIfNeeded also in retrieveReleaseInfo
    also slightly fix&improve echo output in nodeIfNeeeded
    
    maven change "local" version from 1.0.0 to 3.0.0
    https://github.com/metasfresh/metasfresh/issues/2102

--- vars/nodeIfNeeded.groovy
+++ vars/nodeIfNeeded.groovy
@@ -6,14 +6,15 @@ def call(String nodeLabel, Closure body)
     final def labels = env.NODE_LABELS.tokenize(' ')
     if(!labels.contains(nodeLabel))
     {
-      error "Our current node ${env.NODE_NAME} lacks the \'${nodeLabel}\' label. Please make sure to explicitly run on a '${nodeLabel}' node, or on no node at all, to leave it to this step"
+      error "nodeIfNeeded: Our current node ${env.NODE_NAME} lacks the \'${nodeLabel}\' label. Please make sure to explicitly run on a '${nodeLabel}' node, or on no node at all, to leave it to this step"
     }
     // we are on a 'nodeLabel' node
-    echo "retrieveReleaseInfo is called on node ${env.NODE_NAME}"
+    echo "nodeIfNeeded: we are called on node ${env.NODE_NAME}; executing given closure without creating another node."
+
     return body()
   }
   // we are not winthin a node, so start one now
-  echo 'retrieveReleaseInfo is not called on a node, so we now open our own node block'
+  echo 'nodeIfNeeded: we are not called on a node, so we now open our own node block to execute the given closure'
   node(nodeLabel)
   {
     return body()
