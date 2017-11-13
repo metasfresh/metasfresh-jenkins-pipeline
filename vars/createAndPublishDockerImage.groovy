033287b6b42bb3b04b6c1f8054889de2d6288a94
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-11-13 08:43:13 +0100

    * add an additionalBuildArgs parameter (default = '') that is passed to the docker build * remove the docker prefix from all argument names * use the docker repo passed via the dockerRepositoryName argument, without prepending '-dev'

--- vars/createAndPublishDockerImage.groovy
+++ vars/createAndPublishDockerImage.groovy
@@ -1,45 +1,45 @@
 #!/usr/bin/groovy
 
 String call(
-  final String dockerRepositoryName,
-  final String dockerModuleDir,
-  final String dockerBranchName,
-  final String dockerVersionSuffix)
+  final String publishRepositoryName,
+  final String moduleDir,
+  final String branchName,
+  final String versionSuffix,
+  final String additionalBuildArgs = '')
 {
   return createAndPublishDockerImage(
-    dockerRepositoryName,
-    dockerModuleDir,
-    dockerBranchName,
-    dockerVersionSuffix
+    publishRepositoryName,
+    moduleDir,
+    branchName,
+    versionSuffix,
+    additionalBuildArgs
   )
 }
 
 private String createAndPublishDockerImage(
-				final String dockerRepositoryName,
-				final String dockerModuleDir,
-				final String dockerBranchName,
-				final String dockerVersionSuffix)
+				final String publishRepositoryName,
+				final String moduleDir,
+				final String branchName,
+				final String versionSuffix,
+				final String additionalBuildArgs)
 {
-	final dockerWorkDir="${dockerModuleDir}/target/docker"
+	final dockerWorkDir="${moduleDir}/target/docker"
 
 	final def misc = new de.metas.jenkins.Misc();
 
-	final dockerName = "metasfresh/${dockerRepositoryName}-dev"
-	final buildSpecificDockerTag = misc.mkDockerTag("${dockerBranchName}-${dockerVersionSuffix}")
+	final dockerName = "metasfresh/${publishRepositoryName}"
+	final buildSpecificDockerTag = misc.mkDockerTag("${branchName}-${versionSuffix}")
 
 	docker.withRegistry('https://index.docker.io/v1/', 'dockerhub_metasfresh')
 	{
+    final String baseImageRepo='metasfresh-report-dev';
+    final String baseImageVersion=''
 		// note: we omit the "-service" in the docker image name, because we also don't have "-service" in the webui-api and backend and it's pretty clear that it is a service
     // note 2: we need the --pull to avoid building with a stale "latest" base image, see https://docs.docker.com/engine/reference/commandline/build/
-    final def app = docker.build dockerName, "--pull ${dockerWorkDir}" 
+    final def app = docker.build dockerName, "--pull ${additionalBuildArgs} ${dockerWorkDir}"
 
-		app.push misc.mkDockerTag("${dockerBranchName}-latest");
+		app.push misc.mkDockerTag("${branchName}-latest");
 		app.push buildSpecificDockerTag
-		if(dockerBranchName == 'release')
-		{
-			echo 'dockerBranchName == release, so we also push this with the "latest" tag'
-			app.push misc.mkDockerTag('latest');
-		}
 	}
 
 	return "${dockerName}:${buildSpecificDockerTag}"
