60bd412f521340bf13d74e9a250e8bc467af1ac7
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-11-13 10:13:24 +0100

    start implementing docker push without defunct jenkins support
    see https://issues.jenkins-ci.org/browse/JENKINS-46105
    
    standalone report service https://github.com/metasfresh/metasfresh/issues/2932

--- vars/createAndPublishDockerImage.groovy
+++ vars/createAndPublishDockerImage.groovy
@@ -39,19 +39,31 @@ private String createAndPublishDockerImage(
   final imageName = "metasfresh/${publishRepositoryName}:${buildSpecificDockerTag}"
   echo "The docker image name we will push is ${imageName}"
 
-	docker.withRegistry('https://index.docker.io/v1/', 'dockerhub_metasfresh')
-	{
-    final String baseImageRepo='metasfresh-report-dev';
-    final String baseImageVersion=''
-		// note: we omit the "-service" in the docker image name, because we also don't have "-service" in the webui-api and backend and it's pretty clear that it is a service
-    // note 2: we need the --pull to avoid building with a stale "latest" base image, see https://docs.docker.com/engine/reference/commandline/build/
-    final def app = docker.build imageName, "--pull ${additionalBuildArgs} ${dockerWorkDir}"
-    app.push
-
-    final String additionalLatestTag = misc.mkDockerTag("${branchName}-latest")
-    echo "Also pushing this image with tag ${additionalLatestTag}"
-		app.push additionalLatestTag
-	}
-
+  // this stuff doesn't work when we configure the base image version from outside
+  // see https://issues.jenkins-ci.org/browse/JENKINS-46105
+  //performWithJenkinsDockerSupport(imageName, branchName, dockerWorkDir, additionalBuildArgs)
+  withCredentials([usernamePassword(credentialsId: 'dockerhub_metasfresh', passwordVariable: 'dockerRegistryPassword', usernameVariable: 'dockerRegistryUserName')])
+  {
+    sh "docker login --username ${dockerRegistryUserName} --password ${dockerRegistryPassword}"
+    sh "docker build --pull -t ${imageName} ${additionalBuildArgs} ${dockerWorkDir}"
+    sh "docker push index.docker.io/${imageName}"
+  }
 	return imageName
+}
+
+performWithJenkinsDockerSupport(final String imageName,
+  final String branchName,
+  final String dockerWorkDir,
+  final String additionalBuildArgs)
+{
+  	docker.withRegistry('https://index.docker.io/v1/', 'dockerhub_metasfresh')
+  	{
+  		// note: we omit the "-service" in the docker image name, because we also don't have "-service" in the webui-api and backend and it's pretty clear that it is a service
+      // note 2: we need the --pull to avoid building with a stale "latest" base image, see https://docs.docker.com/engine/reference/commandline/build/
+      final def app = docker.build imageName, "--pull ${additionalBuildArgs} ${dockerWorkDir}"
+      app.push
+
+      final String additionalLatestTag = misc.mkDockerTag("${branchName}-latest")
+  		app.push additionalLatestTag
+  	}
 }
