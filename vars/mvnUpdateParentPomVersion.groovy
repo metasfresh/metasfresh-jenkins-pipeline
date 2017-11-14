de687019edc071e90a81f1f285e401bd6dbb2897
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-11-14 10:45:35 +0100

    minor update

--- vars/mvnUpdateParentPomVersion.groovy
+++ vars/mvnUpdateParentPomVersion.groovy
@@ -12,9 +12,10 @@ def call(final MvnConf mvnConf)
     echo "mvnUpdateParentPomVersion is called with mvnConf=${mvnConf}"
 
     // make sure we know which plugin version we run
-    final String versionsPlugin='org.codehaus.mojo:versions-maven-plugin:2.4'
+    final String versionsPlugin='org.codehaus.mojo:versions-maven-plugin:2.5'
 
     // this method is *just* about the parent pom. don't do unexpected additional stuff
+    // don't process the dpendencies and dependencyManagement *section* of the pom
     final String processOnlyParentParams="-DprocessParent=true -DprocessDependencies=false -DprocessDependencyManagement=false -DprocessProperties=false"
 
     echo "mvnUpdateParentPomVersion: Resolve the parent version range"
