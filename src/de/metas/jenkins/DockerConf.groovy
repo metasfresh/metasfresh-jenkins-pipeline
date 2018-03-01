9814caca7bc2f63923bf89f991bafcb2f52e4fa7
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-03-01 14:42:47 +0100

    WIP push what i made so far
    don't expect it to work
    Refactor docker related code https://github.com/metasfresh/metasfresh-jenkins-pipeline/issues/5

---
+++ src/de/metas/jenkins/DockerConf.groovy
@@ -0,0 +1,122 @@
+package de.metas.jenkins
+
+class DockerConf implements Serializable
+{
+	/**
+	  * Mandatory. Examples: metasfresh-app, metasfresh-material-dispo.
+		* This string will be made docker-compliant if necessary. Also, <code>metasfresh/</code> will be prepended
+		*/ 
+	final String artifactName
+
+	/**
+	  * Mandatory: the branch we are building a docker image for. Will go into the tag the images is pushed with.
+		* This string will be made docker-compliant if necessary.
+		*/ 
+	final String branchName
+
+	/**
+	  * Mandatory: the build version we are building a docker image for. Will go into the tag the images is pushed with.
+		* This string will be made docker-compliant if necessary.
+		*/
+	final String versionSuffix
+
+	/**
+	  * Optional: name of path of the docker file, to be concatenated to a "--file .." parameter. 
+		* If a relative path is given, then it's relative to dockerWorkDir.
+	  */
+	final String dockerFile
+
+	/**
+	  * Optional: directory in which the build will be done.
+		*/
+	final String workDir
+
+	/**
+	  * Optional: the docker registry from which we pull.
+		* Example: <code>nexus.metasfresh.com:6000</code>
+	  */
+	final String pullRegistry
+
+	final String pullRegistryCredentialsId
+
+	/**
+	  * Optional: the docker registry to which we push.
+		* Example: <code>nexus.metasfresh.com:6001</code>
+	  */
+	final String pushRegistry
+
+	final String pushRegistryCredentialsId
+
+	/**
+	  * Optional
+	  */
+	final String additionalBuildArgs
+
+	DockerConf(
+			String artifactName,
+			String branchName,
+			String versionSuffix,
+			String workDir = '.',
+			String additionalBuildArgs = '',
+			String dockerFile = 'Dockerfile',
+			String pullRegistry = 'nexus.metasfresh.com:6000',
+			String pullRegistryCredentialsId = 'nexus.metasfresh.com_jenkins',
+			String pushRegistry = 'nexus.metasfresh.com:6001',
+			String pushRegistryCredentialsId = 'nexus.metasfresh.com_jenkins')
+	{
+		this.artifactName = artifactName
+		this.branchName = branchName
+		this.versionSuffix = versionSuffix
+		this.workDir = workDir
+		this.additionalBuildArgs = additionalBuildArgs
+		this.dockerFile = dockerFile
+		this.pullRegistry = pullRegistry
+		this.pullRegistryCredentialsId = pullRegistryCredentialsId
+		this.pushRegistry = pushRegistry
+		this.pushRegistryCredentialsId = pushRegistryCredentialsId
+	}
+
+	DockerConf withArtifactName(String altArtifactName)
+	{
+		return new DockerConf(
+			altArtifactName,
+			this.branchName,
+			this.versionSuffix,
+			this.workDir,
+			this.additionalBuildArgs, 
+			this.dockerFile,
+			this.pullRegistry,
+			this.pullRegistryCredentialsId,
+			this.pushRegistry,
+			this.pushRegistryCredentialsId)
+	}
+
+	DockerConf withWorkDir(String altWorkDir)
+	{
+		return new DockerConf(
+			this.altArtifactName,
+			this.branchName,
+			this.versionSuffix,
+			altWorkDir,
+			this.additionalBuildArgs, 
+			this.dockerFile,
+			this.pullRegistry,
+			this.pullRegistryCredentialsId,
+			this.pushRegistry,
+			this.pushRegistryCredentialsId)
+	}
+	
+
+	String toString()
+	{
+		return """DockerConf[
+  dockerFile=${dockerFile},
+  workDir=${workDir},
+  pullRegistry=${pullRegistry},
+  pushRegistry=${pushRegistry},
+  additionalBuildArgs=${additionalBuildArgs},
+	branchName=${branchName},
+	versionSuffix=${versionSuffix}
+]""";
+	}
+}
---
+++ vars/dockerBuildAndPush.groovy
@@ -0,0 +1,48 @@
+#!/usr/bin/groovy
+
+String call(final DockerConf dockerConf)
+{
+  return buildAndPush(dockerConf)
+}
+
+private String buildAndPush(final DockerConf dockerConf)
+{
+  echo "buildAndPush is called with dockerConf=${dockerConf}"
+
+	final def misc = new de.metas.jenkins.Misc()
+
+
+  final String imageName = "metasfresh/${dockerConf.artifactName}"
+	final String buildSpecificTag = misc.mkDockerTag("${dockerConf.branchName}-${dockerConf.versionSuffix}")
+  final String latestTag = misc.mkDockerTag("${branchName}-LATEST")
+
+  echo "The docker image name we will build and push is ${imageName}"
+
+  def image
+  docker.withRegistry("https://${dockerConf.pullRegistry}/v2/", dockerConf.pullRegistryCredentialsId)
+  {
+    image = docker.build("${imageName}:${buildSpecificTag}", "--pull ${additionalBuildArgs} ${dockerWorkDir}")
+  }
+
+  docker.withRegistry("https://${dockerConf.pushRegistry}/v2/", dockerConf.pushRegistryCredentialsId)
+  {
+    image.push(buildSpecificTag)
+
+    // Also publish a branch specific "LATEST".
+    // Use uppercase because this way it's the same keyword that we use in maven.
+    // Downstream jobs might look for "LATEST" in their base image tag
+    image.push(latestTag)
+
+    if(branchName=='release')
+    {
+      echo 'branchName=release, so we also push this with the "latest" tag'
+      image.push('latest');
+    }
+  }
+
+  // cleanup to avoid disk space issues
+  //  sh "docker rmi ${imageName}:${latestTag}"
+  //  sh "docker rmi ${imageNameWithTag}"
+
+  return imageNameWithTag
+}
