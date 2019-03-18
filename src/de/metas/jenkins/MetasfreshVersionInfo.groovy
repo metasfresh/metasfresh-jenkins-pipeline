627fa57bce0b4c6007584ded0bc29d946bebffb9
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-03-18 11:29:18 +0100

    add mesafresh-admin docker image field
    https://github.com/metasfresh/metasfresh-e2e/issues/9 Extend build system to forward (e2e-)version info

--- src/de/metas/jenkins/MetasfreshVersionInfo.groovy
+++ src/de/metas/jenkins/MetasfreshVersionInfo.groovy
@@ -3,25 +3,31 @@ package de.metas.jenkins
 class MetasfreshVersionInfo implements Serializable
 {
     final String metasfreshVersion;
-    final String metasfreshProcurementWebuiVersion;
-    final String metasfreshWebuiApiVersion;
-    final String metasfreshWebuiFrontendVersion;
-    final String metasfreshE2eDockerImage;
-    final String metasfreshEdiDockerImage;
+    final String procurementWebuiVersion;
+    final String webuiApiVersion;
+    final String webuiFrontendVersion;
+	final String reportDockerBaseImage;
+	final String adminDockerImage;
+    final String ediDockerImage;
+    final String e2eDockerImage;
 
     DockerConf(
 		String metasfreshVersion,
-		String metasfreshProcurementWebuiVersion,
-		String metasfreshWebuiApiVersion,
-		String metasfreshWebuiFrontendVersion,
-		String metasfreshE2eDockerImage,
-        String metasfreshEdiDockerImage)
+		String procurementWebuiVersion,
+		String webuiApiVersion,
+		String webuiFrontendVersion,
+		String reportDockerBaseImage,
+		String adminDockerImage,
+        String ediDockerImage,
+		String e2eDockerImage)
 	{
 		this.metasfreshVersion = metasfreshVersion
-		this.metasfreshProcurementWebuiVersion = metasfreshProcurementWebuiVersion
-		this.metasfreshWebuiApiVersion = metasfreshWebuiApiVersion
-		this.metasfreshWebuiFrontendVersion = metasfreshWebuiFrontendVersion
-		this.metasfreshE2eDockerImage = metasfreshE2eDockerImage
-        this.metasfreshEdiDockerImage = metasfreshEdiDockerImage
+		this.procurementWebuiVersion = procurementWebuiVersion
+		this.webuiApiVersion = webuiApiVersion
+		this.webuiFrontendVersion = webuiFrontendVersion
+		this.reportDockerBaseImage = reportDockerBaseImage
+		this.adminDockerImage = adminDockerImage
+        this.ediDockerImage = ediDockerImage
+		this.e2eDockerImage = e2eDockerImage
 	}
 }
