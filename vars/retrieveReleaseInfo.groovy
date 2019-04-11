240929761f8a597847a0a441a57a9c69913b3d13
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-04-11 17:32:47 +0200

    attempts to get release info via clone

--- vars/retrieveReleaseInfo.groovy
+++ vars/retrieveReleaseInfo.groovy
@@ -11,13 +11,16 @@ String call(final String branchName)
   echo "Attempting to retrive the latest release-info.properties for effectiveBranchName=${effectiveBranchName}"
   nodeIfNeeded('linux', {
     // use -O so a possible stale file is overwritten
-    sh "wget https://raw.githubusercontent.com/metasfresh/metasfresh-release-info/${effectiveBranchName}/release-info.properties --no-check-certificate -O release-info.properties"
+    //sh "wget https://raw.githubusercontent.com/metasfresh/metasfresh-release-info/${effectiveBranchName}/release-info.properties --no-check-certificate -O release-info.properties"
     // note that readProperties also needs to be within the same node block t make sure we actually have access to the downloaded file
-    final Properties props = readProperties file: 'release-info.properties'
 
-    final String releaseVersion = props."release.version"
-    echo "Succeeded to load the following props: ${props}; return only release.version=${releaseVersion} for now as noone uses the other(s)."
-
-    return releaseVersion
+    dir('release-info') 
+    {
+        sh "git clone --depth 1 https://github.com/metasfresh/metasfresh-release-info.git -b ${branchName}"
+        final Properties props = readProperties file: 'metasfresh-release-info/release-info.properties'
+        final String releaseVersion = props."release.version"
+        echo "Succeeded to load the following props: ${props}; return only release.version=${releaseVersion} for now as noone uses the other(s)."
+        return releaseVersion
+    }
   })
 }
