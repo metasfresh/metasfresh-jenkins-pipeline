efeb8240c6a84a25f316ca4b620e0525d19a1b42
metas-jb <julian.bischof@metasfresh.com>
2019-10-09 14:41:10 +0200

    change escaping and making curl more verbose

--- vars/nexusCreateRepoIfNotExists.groovy
+++ vars/nexusCreateRepoIfNotExists.groovy
@@ -40,7 +40,7 @@ void createRepo(final String mvnRepoBaseURL, final String mvnRepoName)
 		
 		
 		echo "Create the repository ${mvnRepoName}-releases and group ${mvnRepoName}";
-		final String createGroupPayload = """{
+		final String createGroupPayload = "{
   "mvn_reponame": "${mvnRepoName}-releases",
   "write_policy": "allow_once",
   "version_policy": "release",
@@ -51,10 +51,10 @@ void createRepo(final String mvnRepoBaseURL, final String mvnRepoName)
   "mvn_groupname": "${mvnRepoName}",
   "group_members": ["maven-public", "${mvnRepoName}-releases"]
 }
-"""
+"
 
 		// # nexus ignored application/json
-		final String createGroupCommand =  "curl --silent -H \"Content-Type: text/plain\" -X POST -u ${NEXUS_LOGIN} -d \'${createGroupPayload}\' ${mvnRepoBaseURL}rest/v1/script/create_maven_repo/run"
+		final String createGroupCommand =  "curl -v --header \"Content-Type: text/plain\" -X POST -u ${NEXUS_LOGIN} ${mvnRepoBaseURL}rest/v1/script/create_maven_repo/run -d \"${createGroupPayload}\""
 		sh "${createGroupCommand}"
 
 }
