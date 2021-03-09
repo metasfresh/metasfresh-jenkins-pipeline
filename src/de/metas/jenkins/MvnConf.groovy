554ea3a2c30833b64c6817101453003e867c45b2
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2021-03-09 10:58:15 +0100

    start introducing an improved bpartner-rest endpoint v2
    * rename packages from `...` to `....v1` and copying code to to `....v2`
    * remove externalId from JSON
    * do the same for SyncAdvise and replace UPDATE_DELETE and UPDATE_MERGE with UPDATE (with the semantics of UPDATE_MERGE)
    
    https://github.com/metasfresh/metasfresh/issues/10784

--- src/de/metas/jenkins/MvnConf.groovy
+++ src/de/metas/jenkins/MvnConf.groovy
@@ -1,106 +1,98 @@
 package de.metas.jenkins
 
-class MvnConf implements Serializable
-{
-	final String MF_MAVEN_REPO_ID = "metasfresh-task-repo"
+class MvnConf implements Serializable {
+    final String MF_MAVEN_REPO_ID = "metasfresh-task-repo"
 
 
-	/**
-	  * String containing the pom file, to be concatenated to a "--file .." parameter
-	  */
-	final String pomFile
+    /**
+     * String containing the pom file, to be concatenated to a "--file .." parameter
+     */
+    final String pomFile
 
-	/**
-	  * String containing the settings file, to be contatenated to a "--settings .." parameter
-		*/
-	final String settingsFile
+    /**
+     * String containing the settings file, to be contatenated to a "--settings .." parameter
+     */
+    final String settingsFile
 
-	final String mvnRepoName
+    final String mvnRepoName
 
-	final String mvnResolveRepoBaseURL
+    final String mvnResolveRepoBaseURL
 
-	final String mvnDeployRepoBaseURL
+    final String mvnDeployRepoBaseURL
 
-	/**
-	  * Creates a new instance.
-		*
-		* @param pomFile example 1: 'pom.xml'; example 2: 'de.metas.reactor/pom.xml'
-		* @param settingsFile the settings.xml file
-		* @param mvnRepoName example 'gh2102-mf'
-		* @param mvnResolveRepoBaseURL; used for both the resolve and deployment related URLs exmaple 'https://repo.metasfresh.com'
-		*/
-	MvnConf(
-			String pomFile,
-			String settingsFile,
-			String mvnRepoName,
-			String mvnRepoBaseURL
-			)
-	{
-		this.pomFile = pomFile
-		this.settingsFile = settingsFile
-		this.mvnRepoName = mvnRepoName
-		this.mvnResolveRepoBaseURL = mvnRepoBaseURL
-		this.mvnDeployRepoBaseURL = mvnRepoBaseURL
-	}
+    /**
+     * Creates a new instance.
+     *
+     * @param pomFile example 1: 'pom.xml'; example 2: 'de.metas.reactor/pom.xml'
+     * @param settingsFile the settings.xml file
+     * @param mvnRepoName example 'gh2102-mf'
+     * @param mvnResolveRepoBaseURL ; used for both the resolve and deployment related URLs exmaple 'https://repo.metasfresh.com'
+     */
+    MvnConf(
+            String pomFile,
+            String settingsFile,
+            String mvnRepoName,
+            String mvnRepoBaseURL
+    ) {
+        this.pomFile = pomFile
+        this.settingsFile = settingsFile
+        this.mvnRepoName = mvnRepoName
+        this.mvnResolveRepoBaseURL = mvnRepoBaseURL
+        this.mvnDeployRepoBaseURL = mvnRepoBaseURL
+    }
 
-	/**
-	  * Similar to the other constructor, but allows deployment and resolve repos to differ
-	  */
-	MvnConf(
-			String pomFile,
-			String settingsFile,
-			String mvnRepoName,
-			String mvnResolveRepoBaseURL,
-			String mvnDeployRepoBaseURL
-			)
-	{
-		this.pomFile = pomFile
-		this.settingsFile = settingsFile
-		this.mvnRepoName = mvnRepoName
-		this.mvnResolveRepoBaseURL = mvnResolveRepoBaseURL
-		this.mvnDeployRepoBaseURL = mvnDeployRepoBaseURL
-	}
+    /**
+     * Similar to the other constructor, but allows deployment and resolve repos to differ
+     */
+    MvnConf(
+            String pomFile,
+            String settingsFile,
+            String mvnRepoName,
+            String mvnResolveRepoBaseURL,
+            String mvnDeployRepoBaseURL
+    ) {
+        this.pomFile = pomFile
+        this.settingsFile = settingsFile
+        this.mvnRepoName = mvnRepoName
+        this.mvnResolveRepoBaseURL = mvnResolveRepoBaseURL
+        this.mvnDeployRepoBaseURL = mvnDeployRepoBaseURL
+    }
 
-	String toString()
-	{
-		return """MvnConf[
+    String toString() {
+        return """MvnConf[
   pomFile=${pomFile},
   settingsFile=${settingsFile},
   mvnRepoName=${mvnRepoName},
   mvnResolveRepoBaseURL=${mvnResolveRepoBaseURL},
   mvnDeployRepoBaseURL=${mvnDeployRepoBaseURL}
 ]""";
-	}
+    }
 
-	String getDeployRepoURL()
-	{
-		return "${this.mvnDeployRepoBaseURL}/repository/${this.mvnRepoName}-releases"
-	}
+    String getDeployRepoURL() {
+        return "${this.mvnDeployRepoBaseURL}/repository/${this.mvnRepoName}-releases"
+    }
 
-	String getResolveRepoURL()
-	{
-		return "${this.mvnResolveRepoBaseURL}/repository/${this.mvnRepoName}"
-	}
+    String getResolveRepoURL() {
+        return "${this.mvnResolveRepoBaseURL}/repository/${this.mvnRepoName}"
+    }
 
-	/**
-	  * @return a string containing a number of "-Dtask-repo-.." parameters, used to resolve dependencies from the repos that we want to resolve from.<br>
-		*		IMPORTANT: please note that these are not "real" maven paramters, but properties which we use in the settings.xml file that is provided by jenkins.
-		*/
-	String getResolveParams()
-	{
-		return "-Dtask-repo-id=${MF_MAVEN_REPO_ID} -Dtask-repo-name=\"${this.mvnRepoName}\" -Dtask-repo-url=\"${this.resolveRepoURL}\"";
-	}
+    /**
+     * @return a string containing a number of "-Dtask-repo-.." parameters, used to resolve dependencies from the repos that we want to resolve from.<br>
+     * 		IMPORTANT: please note that these are not "real" maven paramters, but properties which we use in the settings.xml file that is provided by jenkins.
+     */
+    String getResolveParams() {
+        return "-Dtask-repo-id=${MF_MAVEN_REPO_ID} -Dtask-repo-name=\"${this.mvnRepoName}\" -Dtask-repo-url=\"${this.resolveRepoURL}\"";
+    }
 
-	/**
-	  * String containing a "-DaltDeploymentRepository=.." maven parameter, used to deploy dependencies to the repo which we want to deploy to
-		*/
-	String getDeployParam()
-	{
-		return "-DretryFailedDeploymentCount=5 -DaltDeploymentRepository=\"${MF_MAVEN_REPO_ID}::default::${getDeployRepoURL()}\"";
-	}
+    /**
+     * String containing a "-DaltDeploymentRepository=.." maven parameter, used to deploy dependencies to the repo which we want to deploy to.
+     * Note that credentials are provided by jenkins "Config File Management" plugin
+     */
+    String getDeployParam() {
+        return "-DretryFailedDeploymentCount=5 -DaltDeploymentRepository=\"${MF_MAVEN_REPO_ID}::default::${getDeployRepoURL()}\"";
+    }
 
-	MvnConf withPomFile(String pomFile)
-	{
-		return new MvnConf(pomFile,	this.settingsFile, this.mvnRepoName, this.mvnResolveRepoBaseURL, this.mvnDeployRepoBaseURL)
-	}
+    MvnConf withPomFile(String pomFile) {
+        return new MvnConf(pomFile, this.settingsFile, this.mvnRepoName, this.mvnResolveRepoBaseURL, this.mvnDeployRepoBaseURL)
+    }
 }
